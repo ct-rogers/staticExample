72d92194fc5ab3499d8309e9a4b8dffebe3161c7
Mark Dewey <mark.t.dewey@gmail.com>
2017-08-14 16:12:57 -0400

    first

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace staticExample
+{
+
+    public class Person
+    {
+        public string Name { get; set; }
+        private static int Population { get; set; }
+
+        public Person()
+        {
+            Person.IncreasePopulation();
+        }
+       
+        public void SayHello()
+        {
+            Console.WriteLine("Hello my name is " + this.Name);
+        }
+
+        public static void IncreasePopulation()
+        {
+            Person.Population++;
+        }
+
+        public static int GetPopulation()
+        {
+            return Person.Population;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var paul = new Person{Name = "Paul"};
+            var steve = new Person{Name = "Steve"};
+
+            steve.SayHello();
+
+            Console.WriteLine(Person.GetPopulation());
+
+        }
+    }
+}
